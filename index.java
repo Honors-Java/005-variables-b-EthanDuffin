4230f6f809f278633644daefaa52664232bf4ecf
Ethan Duffin <ethan.duffin@icloud.com>
2020-10-20 14:46:55 +0000

    I fixed an issue with the ellipses not being drawn over the rectangle.

--- index.java
+++ index.java
@@ -9,35 +9,26 @@ int b1 = random(1,255);
 
 void setup() {
 	size(500, 500);
-  rect(CENTER);
   ellipseMode(CENTER);
   rectMode(CENTER);
-  frameRate(30);
+  frameRate(60);
 }
 
 void mousePressed(){
-fill(random(0,255), random(0,255), random(0,255));
-rect(random(0,500), random(0,500), random(200,300), random(200,300));
-}
-
-void mousePressed(){
-fill(random(0,255), random(0,255), random(0,255));
-
+var1 = (int) random(0, 500);
+var2 = (int) random(0, 500);
+var3 = (int) random(50, 400);
+var4 = (int) random(50, 400);
 background(250);
-var1 = random(0, 500);
-var2 = random(0, 500);
-var3 = random(50, 400);
-var4 = random(50, 400);
-
-r1 = random(1,255);
-g1 = random(1,255);
-b1 = random(1,255);
+r1 = (int) random(1,255);
+g1 = (int) random(1,255);
+b1 = (int) random(1,255);
 }
 
 void draw() {
-println(mouseX, mouseY);
-fill(random(0,255), random(0,255), random(0,255));
-ellipse(mouseX - random(-30,30), mouseY - random(-30,30), 10, 10);
+stroke(0);
 fill(r1, g1, b1);
 rect(var1, var2, var3, var4);
+fill((int) random(0, 255), (int) random(0, 255), (int) random(0, 255));
+ellipse(mouseX + (int) random(-30, 30), mouseY + (int) random(-30, 30), 10, 10);
 }
